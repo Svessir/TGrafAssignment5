39ade8c0b874f12d1691b923839ba59fb5286fc4
sverrir <sverrir1993@gmail.com>
2016-10-15 21:46:49 +0000

    initial

---
+++ desktop/src/is/ru/graphics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package is.ru.graphics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import is.ru.graphics.EarthProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EarthProject(), config);
+	}
+}
